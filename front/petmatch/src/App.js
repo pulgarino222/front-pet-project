968f1fafabeec29cfef2f05ae66430974b9e5c19
andres1988 <a.canorave@gmail.com>
2024-09-17 15:03:41 -0500

    create App.js

--- front/petmatch/src/App.js
+++ front/petmatch/src/App.js
@@ -1,1 +1,14 @@
-//pending
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./Routes/Routes";
+
+function App() {
+
+    return (
+        <Router>
+            <AppRoutes />
+        </Router>
+    );
+}
+
+export default App;
