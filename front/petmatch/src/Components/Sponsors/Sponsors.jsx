f96bcef8b1a03b05879b074576c6122a83f4549f
Deybison Sepulveda <camimilo2014@gmail.com>
2024-09-20 00:04:21 -0500

    changes

--- front/petmatch/src/Components/Sponsors/Sponsors.jsx
+++ front/petmatch/src/Components/Sponsors/Sponsors.jsx
@@ -4,7 +4,7 @@ import "aos/dist/aos.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import SponsorCard from "./TestimonialCard";
+import SponsorCard from "./SponsorCard";
 
 
 // Importa las imágenes locales
