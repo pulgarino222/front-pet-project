092cbf6e6d39fdca85d48653a24117c7107650eb
Deybison Sepulveda <camimilo2014@gmail.com>
2024-09-19 23:49:43 -0500

    structure change

--- front/petmatch/src/Components/Testimonials/Testimonials.jsx
+++ front/petmatch/src/Components/Testimonials/Testimonials.jsx
@@ -4,7 +4,7 @@ import "aos/dist/aos.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import SponsorCard from "./SponsorCard";
+import SponsorCard from "./TestimonialCard";
 
 
 // Importa las imágenes locales
